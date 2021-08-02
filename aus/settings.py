619b162a0e606388f90a5420c2d173c2f93ce7e7
fullstacksuiit <rahmantanshit@gmail.com>
2021-08-02 22:26:19 +0530

    settings

--- aus/settings.py
+++ aus/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '21vbc+93bw7%5!v*@x6wdfrd9m7)ayi3+mjmw%el*r4uuk1=!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
