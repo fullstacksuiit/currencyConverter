6d7f41ed2d4abb96ae23732325b2054561d0eb31
fullstacksuiit <rahmantanshit@gmail.com>
2021-08-02 22:18:15 +0530

    urls

--- aus/urls.py
+++ aus/urls.py
@@ -4,8 +4,8 @@ from currency.views import historical_currency, latest_currency, index, history
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('history', history),
+    path('', index, name='lastest'),
+    path('history', history, name='history'),
     path('latest', latest_currency),
     path('historical', historical_currency),
 ]
